4d1856a2389647656425624e5918e5b4f1502d3f
Cristian523 <cl.cristian100@gmail.com>
2026-05-18 17:32:51 -0300

    Adição de teste.c, data_structures.h e Makefile
    1) data_structures.h contém todos os includes das estruturas de dados até então implementadas
    2) teste.c é um arquivo em que eu testo as estruturas de dados.
    3) O arquivo Makefile é por onde eu consigo executar todos esses códigos por meio do terminal do linux

---
+++ Data_structures/data_structures.h
@@ -0,0 +1,17 @@
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
+
+#include "c_string.h"
+#include "vector.h"
+#include "vector_str.h"
+#include "linked_list.h"
+#include "linked_list_str.h"
+#include "stack.h"
+#include "stack_str.h"
+#include "queue.h"
+#include "queue_str.h"
+#include "hash_table.h"
+#include "hash_table_sls.h"
+#include "hash_table_sn.h"
+
+#endif
---
+++ Data_structures/teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "data_structures.h"
+#define BOOL(x) ((x) ? "true" : "false")
+
+int main() {
+    
+    
+    return 0;
+}
